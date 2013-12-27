656e85e20f73caf3e0511a42a694c4e7b07109d3
TribeX <Tribex@users.noreply.github.com>
2013-12-27 20:21:25 +0800

    Preparation for modular styles.

---
+++ src/jsStdLib/preload/styles/0_init.js
@@ -0,0 +1,9 @@
+/** --styles/0_init.js (Preload)--
+ * Creates the styles object for styling and layout of widgets.
+ * The 0 is used to force the file to be loaded first.
+ */
+
+var styles = {
+		
+		registerStyle : function();
+}
