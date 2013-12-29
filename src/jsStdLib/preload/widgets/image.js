6dc272ccac26dc63ab8476f1b2041025cf8805cd
Nateowami <nateowami@gmail.com>
2013-12-29 18:31:41 +0800

    Defined <img>; added it to init.rsm

---
+++ src/jsStdLib/preload/widgets/image.js
@@ -0,0 +1,27 @@
+/**
+ * Provides the ability to use XML img tags to create images.
+ */
+
+require(Packages.java.awt.image.BufferedImage);
+require(Packages.java.io.File);
+require(Packages.java.io.IOException);
+require(Packages.javax.imageio.ImageIO);
+require(Packages.javax.swing.ImageIcon);
+require(Packages.javax.swing.JFrame);
+require(Packages.javax.swing.JLabel);
+
+widgets.registerWidget("Image", "img", "A simple image", function (parentComposite, widgetElement, engine) {
+	
+	//if the src is there
+	if (widgetElement.getAttributeNode("src") != null) {
+		//create a new image object
+		var image = new ImageIO.read(new File(widgetElement.getAttributeNode("src").getNodeValue()));
+	}
+
+	var widget = new JLabel(new ImageIcon(image));
+	parentComposite.add(widget, Layouts.getWidgetConstraint(widgetElement));
+
+	widgets.initializeWidget(widget, widgetElement, engine);
+	return widget;
+});
+
