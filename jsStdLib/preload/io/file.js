994d2edd1735c802456bbf0def69cc9d76bada15
Nateowami <nateowami@gmail.com>
2014-01-03 16:26:58 +0800

    Added file io abilities

--- jsStdLib/preload/io/file.js
+++ jsStdLib/preload/io/file.js
@@ -1,29 +1,37 @@
 //pretends to try to think it almost half starts to have a buggy way to almost kinda read a file (pending)
 
 //import packages
-require(Packages.io.File);
-require(Packages.util.Scanner);
+require(Packages.java.io.File);
+require(Packages.java.util.Scanner);
 
 //creates a new file
-function file{
-	
+function file(filename){
+
+	//instintate the methods
+	this.getNextLine = getNextLine;
+	this.getNext = getNext;
+	this.hasNext = hasNext;	
+
 	//create a new scanner from the file name
-	this.scanner = new Scanner(new File(filename));			
-	
-	//finds if there's anything more in the file
-	function hasNext(){
-		return scanner.hasNext();	
+	try{
+		this.scanner = new Scanner(new File(filename));			
+	}
+	catch (err){ 
+		console.log(err.message);
+	}
+}
+
+//finds if there's anything left in the file
+function hasNext(){
+		return this.scanner.hasNext();	
 	}
 
-	//finds the next word in the file
-	function getNext(){
-		return scanner.getNext();
-	}
+//finds the next word in the file
+function getNext(){
+	return this.scanner.next();
+}
 
-	//gets the next line in the file
-	function getNextLine(){
-		return scanner.getNextLine();
-	}
-
-	s//more functions to come after debug
+//gets the next line in the file
+function getNextLine(){
+	return this.scanner.nextLine();
 }
