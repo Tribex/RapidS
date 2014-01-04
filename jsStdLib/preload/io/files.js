628d2b7e1e6708e267faa9cc87c4cc673685624a
TribeX <Tribex@users.noreply.github.com>
2014-01-04 13:34:36 +0800

    Added many more file functions: files.move() files.cut() - alias of move files.mkdir() files.exists() files.find() files.size() files.tempdir - The system temporary directory. files.userdir - The user's home directory.

--- jsStdLib/preload/io/files.js
+++ jsStdLib/preload/io/files.js
@@ -12,7 +12,7 @@ require(Packages.org.apache.commons.io.FileUtils);
 function files(filename, flag) {
 	//The file being operated on
 	var filename = filename;
-	
+
 	//If the file operation specified is "read", "read-write", or "read-write-append" (r, rw, rw+)
 	if (stringContains(flag, "r")) { 
 		try {
@@ -20,22 +20,22 @@ function files(filename, flag) {
 		} catch (e) {
 			Utilities.showError("Error reading file: "+filename+". Does it exist?\n\n"+e.message);
 		}
-		
+
 		//Returns a string containing all the contents of filename
 		this.read = function () {
 			return FileUtils.readFileToString(new File(Globals.getCWD()+filename));
 		}
-		
+
 		//Reads the next line from filename
 		this.readNextLine = function() {
-				if (scanner.hasNextLine()) {
-					return scanner.nextLine();
-				} else {
-					//Return a blank string to prevent errors
-					return "";
-				}
+			if (scanner.hasNextLine()) {
+				return scanner.nextLine();
+			} else {
+				//Return a blank string to prevent errors
+				return "";
+			}
 		}
-		
+
 		//Reads the next work from filename
 		this.readNext = function() {
 			if (scanner.hasNext()) {
@@ -44,18 +44,18 @@ function files(filename, flag) {
 				return "";
 			}
 		}
-		
+
 		//Boolean. Determines whether or not there is another word in filename
 		this.hasNext = function() {
 			return scanner.hasNext();
 		}
-		
+
 		//Boolean. Determines whether or not there is another line in filename
 		this.hasNextLine = function() {
 			return scanner.hasNextLine();
 		}
 	}
-	
+
 	//If the file operation is "write", "write-append", "read-write", or "read-write-append" (w, w+, rw, rw+)
 	if (stringContains(flag, "w")) {
 
@@ -65,16 +65,16 @@ function files(filename, flag) {
 		if (stringContains(flag, "+")) {
 			append = true;
 		}
-		
+
 		//Write 'data' to 'filename'.
 		this.write = function(data) {
 			try {
-				FileUtils.write(new File(Globals.getCWD()+filename), data, append);
+				FileUtils.writeStringToFile(new File(Globals.getCWD()+filename), data, append);
 			} catch (e) {
 				Utilities.showError("Error writing to file: "+filename+" - "+e.message);
 			}
 		}
-		
+
 		//Write 'data' to 'filename'. Always preserves existing data, and adds a newline to the end of 'data'
 		this.writeLine = function(data) {
 			try {
@@ -96,23 +96,108 @@ files.copy = function(sourceFileName, destFileName) {
 	//Set up Files
 	var sourcefile = new File(Globals.getCWD()+sourceFileName);
 	var destfile = new File(Globals.getCWD()+destFileName);
-	
-	//If the source exists (of course the destination doesn't
+
+	//If the source exists (of course the destination doesn't)
 	if (sourcefile.exists()) {
-		
+
 		//If the source is a directory, copy it as a directory
 		if (sourcefile.isDirectory()) {
 			FileUtils.copyDirectory(sourcefile, destfile);
 			return true;
-		
-		//If the source is a file, or unknown type, attempt to copy it as a file
+
+			//If the source is a file, or unknown type, attempt to copy it as a file
 		} else {
 			FileUtils.copyFile(sourcefile, destfile);
 			return true;
 		}
-		
+
 	} else {
 		Utilities.showError("File: "+sourceFileName+" does not exist!");
 		return false;
 	}
 }
+
+/* Static function for moving a file from one place to another. (cut) */
+files.move = function(sourceFileName, destFileName) {
+	//Set up Files
+	var sourcefile = new File(Globals.getCWD()+sourceFileName);
+	var destfile = new File(Globals.getCWD()+destFileName);
+
+	//If the source exists (of course the destination doesn't)
+	if (sourcefile.exists()) {
+
+		//If the source is a directory, move it as a directory
+		if (sourcefile.isDirectory()) {
+			FileUtils.moveDirectory(sourcefile, destfile);
+			return true;
+
+			//If the source is a file, or unknown type, attempt to move it as a file
+		} else {
+			FileUtils.moveFile(sourcefile, destfile);
+			return true;
+		}
+
+	} else {
+		Utilities.showError("File: "+sourceFileName+" does not exist!");
+		return false;
+	}
+}
+//files.cut, alias of files.move
+files.cut = files.move;
+
+/** Make a directory.
+ * @param dirname The string name of the directory
+ * @param recursive Whether or not to create multiple nested directories as indicated by dirname
+ */
+files.mkdir = function(dirname, recursive) {
+	recursive = typeof recursive !== 'undefined' ? recursive : false;
+
+	dir = new File(Globals.getCWD()+dirname);
+	if (recursive) {
+		if (dir.mkdirs()) {
+			return true;
+		} else {
+			return false;
+		}
+	} else {
+		if (dir.mkdir()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
+/* Returns the size of a file in bytes */ 
+files.size = function(filename) {
+	file = new File(filename);
+	try {
+		return FileUtils.sizeOf(file);
+	} catch (e) {
+		Utilities.showError("File: "+filename+" does not exist.");
+		return false;
+	}
+}
+
+/* List files inside dirname */
+files.find = function(dirname, recursive, extensions) {
+	recursive = typeof recursive !== 'undefined' ? recursive : false;
+	extensions = typeof extensions !== 'undefined' ? extensions : null;
+
+	return FileUtils.listFiles(new File(Globals.getCWD()+dirname), extensions, recursive);
+}
+
+/* Determines whether or not a file exists */
+files.exists = function(filename) {
+	if (new File(Globals.getCWD()+filename).exists()) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/* A string representing the location of the system temporary directory */
+files.tempdir = FileUtils.getTempDirectoryPath();
+
+/* A string representing the location of the user's home directory. MAY MISBEHAVE ON WINDOWS VISTA+ */
+files.userdir = FileUtils.getUserDirectoryPath();
