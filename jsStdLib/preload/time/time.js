49b4634a01d0937a4d2c563a0f80855e5ec3fbaa
TribeX <Tribex@users.noreply.github.com>
2014-01-24 15:59:18 +0800

    Documented time.js (Thanks Nathaniel!) Documentation will be generated and posted to the site soon.

--- jsStdLib/preload/time/time.js
+++ jsStdLib/preload/time/time.js
@@ -1,182 +1,202 @@
-/*
-Provides methods for finding the current date and time
-For sorting purposes larger time periods are at the top,
-while small time periods (seconds) are at the bottom.
-*/
+/**
+ * @file Provides methods for finding the current date and time
+ * For sorting purposes larger time periods are at the top,
+ * while small time periods (seconds) are at the bottom.
+ * @author Nateowami
+ */
 
 //import Calendar
 require(Packages.java.util.Calendar);
 
-//the top level variable
+/**
+ * Global time namespace.
+ * @namespace
+ */
 var time = {};
 
 
-/*
-Returns the current year
-*/
+/**
+ * Gets the current year.
+ * @returns {string} The current year.
+ */
 time.year = function () {
-	return Calendar.getInstance().get(Calendar.YEAR);
+    return Calendar.getInstance().get(Calendar.YEAR);
 }
 
-/*
-Returns the current month of the current year
-*/
+/**
+ * Gets the current month.
+ * @returns {string} The current month of the current year.
+ */
 time.month = function () {
-	return Calendar.getInstance().get(Calendar.MONTH);
+    return Calendar.getInstance().get(Calendar.MONTH);
 }
 
-/*
-Returns the current date of the current month (0-11)
-*/
+/**
+ * Gets the current date of the current month (0-11).
+ * @returns {string} The current day of the current month, in number format.
+ */
 time.date = function () {
-	return Calendar.getInstance().get(Calendar.DATE);
+    return Calendar.getInstance().get(Calendar.DATE);
 }
 
-/*
-Returns the name of the current month, such as "January"
-*/
+/**
+ * Gets the name of the current month, such as "January".
+ * @returns {string} The name of the current month.
+ */
 time.monthName = function () {
-	switch(time.month()){
-		//return is as good as break
-		case 0:
-			return "January";
-		case 1:
-			return "February";
-		case 2:
-			return "March";
-		case 3:
-			return "April";
-		case 4:
-			return "May";
-		case 5:
-			return "June";
-		case 6:
-			return "July";
-		case 7:
-			return "August";
-		case 8:
-			return "September";
-		case 9:
-			return "October";
-		case 10:
-			return "November";
-		case 11:
-			"December";
-	}
+    switch(time.month()){
+    //return is as good as break
+    case 0:
+        return "January";
+    case 1:
+        return "February";
+    case 2:
+        return "March";
+    case 3:
+        return "April";
+    case 4:
+        return "May";
+    case 5:
+        return "June";
+    case 6:
+        return "July";
+    case 7:
+        return "August";
+    case 8:
+        return "September";
+    case 9:
+        return "October";
+    case 10:
+        return "November";
+    case 11:
+        "December";
+    }
 }
 
-/*
-Returns the current day of the current week (1-7)
-*/
+/**
+ * Gets the current day of the current week (1-7)
+ * @returns {string} The current day of the week.
+ */
 time.dayOfWeek = function () {
-	return Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+    return Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
 }
 
-/*
-Returns the name of the day of the week, such as "Monday"
-*/
+/**
+ * Gets the name of the day of the week, such as "Monday".
+ * @returns {string} The name of the current day of the week.
+ */
 time.dayOfWeekName = function () {
-	//return is as good as break
-	switch(time.dayOfWeek()){
-		case 1:
-			return "Sunday";
-		case 2:
-			return "Monday";
-		case 3:
-			return "Tuesday";
-		case 4:
-			return "Wednesday";
-		case 5:
-			return "Thursday";
-		case 6:
-			return "Friday";
-		case 7:
-			return "Saturday";
-	}
+    //return is as good as break
+    switch(time.dayOfWeek()){
+    case 1:
+        return "Sunday";
+    case 2:
+        return "Monday";
+    case 3:
+        return "Tuesday";
+    case 4:
+        return "Wednesday";
+    case 5:
+        return "Thursday";
+    case 6:
+        return "Friday";
+    case 7:
+        return "Saturday";
+    }
 }
 
-/*
-Returns true if it is am, false if it is pm
-*/
+/**
+ * Whether or not the current time is AM.
+ * @returns {boolean} True if it is currently AM, False if it is PM.
+ */
 time.isAM = function () {
-	//XXX bug here!
-	if(Calendar.getInstance().get(Calendar.HOUR_OF_DAY) < 12){
-		return true;
-	}
-	else{
-		return false;
-	}
+    //XXX bug here!
+    //XXX 24HourTime?
+    if(Calendar.getInstance().get(Calendar.HOUR_OF_DAY) < 12){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
-/*
-Returns the hour in 12 hour format
-*/
+/**
+ * Gets the hour in 12 hour format.
+ * @returns {integer} Representing the current hour in 12 hour format.
+ */
 time.hour12 = function () {
-	//gives us 0-11
-	var hours = Calendar.getInstance().get(Calendar.HOUR);
-	//call hour 0, 12
-	if(hours == 0){
-		//in the 12-hour format 0 is shown as 12
-		hours = 12;
-	}
-	return hours;
+    //gives us 0-11
+    var hours = Calendar.getInstance().get(Calendar.HOUR);
+    //call hour 0, 12
+    if(hours == 0){
+        //in the 12-hour format 0 is shown as 12
+        hours = 12;
+    }
+    return hours;
 }
 
-/*
-Returns the hour in 24 hour format
-*/
+/**
+ * Gets the hour in 24 hour format.
+ * @returns {integer} Representing the current hour in 24 hour format.
+ */
 time.hour24 = function () {
-	return Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+    return Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
 }
 
-/*
-Returns the number of minutes past the current hour
-*/
+/**
+ * Gets the number of minutes past the current hour.
+ * @returns {integer} The number of minutes past the current hour.
+ */
 time.minute = function (){
-	return Calendar.getInstance().get(Calendar.MINUTE);
+    return Calendar.getInstance().get(Calendar.MINUTE);
 }
 
-/*
-Returns the number of seconds past the current minute
-*/
+/**
+ * Gets the number of seconds past the current minute.
+ * @returns {integer} The number of seconds past the current minute.
+ */
 time.second = function (){
-	return Calendar.getInstance().get(Calendar.SECOND);
+    return Calendar.getInstance().get(Calendar.SECOND);
 }
 
-/*
-Returns the number of milliseconds past the current second
-*/
+/**
+ * Gets the number of milliseconds past the current second.
+ * @returns {integer} The number of milliseconds past the current second.
+ */
 time.millisecond = function (){
-	return Calendar.getInstance().get(Calendar.MILLISECOND);
+    return Calendar.getInstance().get(Calendar.MILLISECOND);
 }
 
 //Leap-year stuff below
 
-/*
-Returns if the current year is a leap year
-*/
+/**
+ * Whether or not the current year is a leap year
+ * @returns True if the current year is a leap year, False if not.
+ */
 time.isLeapYear = function (){
-	//ask about the current year
-	return time.isLeapYear(time.year());
-}	
+    //ask about the current year
+    return time.isLeapYear(time.year());
+}
 
-/*
-Returns if the specified year is a leap year
-*/
+/**
+ * Whether or not the specified year is a leap year
+ * @param year {integer} The year to check.
+ * @returns True if the specified year is a leap year, False if not.
+ */
 time.isLeapYear = function (year){
-	var leap = false;
-	//these are if's intentionally, not else if's
-	//if it's divisible by four
-	if(year % 4 == 0){
-		leap = true;
-	}
-	//100
-	if(year % 100 == 0){
-		leap = false;
-	}
-	//400
-	if(year % 400 == 0){
-		leap = true;
-	}
-	return leap;
+    var leap = false;
+    //these are if's intentionally, not else if's
+    //if it's divisible by four
+    if(year % 4 == 0){
+        leap = true;
+    }
+    //100
+    if(year % 100 == 0){
+        leap = false;
+    }
+    //400
+    if(year % 400 == 0){
+        leap = true;
+    }
+    return leap;
 }
