159a901cfbf16376195b86dcc0e942ae4612811c
TribeX <Tribex@users.noreply.github.com>
2014-02-04 13:52:11 +0800

    Removed an unused import.

--- src/us/derfers/tribex/rapids/Utilities.java
+++ src/us/derfers/tribex/rapids/Utilities.java
@@ -40,7 +40,6 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.apache.commons.io.FileUtils;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
